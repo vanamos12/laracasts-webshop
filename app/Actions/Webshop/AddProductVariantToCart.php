7f3714db632b7267b4ac6f7c92ff2fd6a41a1ca8
POKA TCHONENG Hermann <pokatchoneng@yahoo.fr>
2024-07-05 13:45:43 +0100

    part 11 persisting our cart item

--- app/Actions/Webshop/AddProductVariantToCart.php
+++ app/Actions/Webshop/AddProductVariantToCart.php
@@ -7,6 +7,20 @@ use App\Models\Cart;
 class AddProductVariantToCart
 {
     public function add($variantId){
+
+        $cart = match(auth()->guest()){
+            true => Cart::firstOrCreate([
+                'session_id' => session()->getId()
+            ]),
+            false => auth()->user()->cart ? : auth()->user()->cart()->create()
+        };
+
+        $cart->items()->create([
+            'product_variant_id' => $variantId,
+            'quantity' => 1,
+        ]);
+        
+        /*
        if (auth()->guest()){
             $cart = Cart::firstOrCreate([
                 'session_id' => session()->getId()
@@ -16,7 +30,8 @@ class AddProductVariantToCart
        if (auth()->user()){
 
            $cart= auth()->user()->cart ? : auth()->user()->cart()->create();
-       } 
+       }
+           */ 
        dd($cart);
     }
 
