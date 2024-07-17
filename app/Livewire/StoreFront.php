1f9297599a5439230cb9282d40108548e5a459f8
POKA TCHONENG Hermann <pokatchoneng@yahoo.fr>
2024-07-17 07:41:39 +0100

    part 33 upgrading to livewire version 3

--- app/Livewire/StoreFront.php
+++ app/Livewire/StoreFront.php
@@ -3,16 +3,22 @@
 namespace App\Livewire;
 
 use App\Models\Product;
+use Livewire\Attributes\Url;
 use Livewire\Component;
 use Livewire\WithPagination;
 
 class StoreFront extends Component
 {
     use WithPagination;
+
+    #[Url()]
+    public $keywords;
     
     public function getProductsProperty(){
         
-        return Product::query()->paginate(1);
+        return Product::query()
+            ->when($this->keywords, fn($query) => $query->where('name', 'like', "%$this->keywords%"))
+            ->paginate(5);
     }
     
     public function render()
--- resources/views/livewire/store-front.blade.php
+++ resources/views/livewire/store-front.blade.php
@@ -1,5 +1,11 @@
-<div>
-<div class="grid grid-cols-4 gap-4 mt-12">
+<div class="mt-12">
+<div class="flex justify-between items-center">
+    <h1 class="text-xl font-medium">Our products</h1>
+    <div>
+        <x-input wire:model.live.debounce="keywords" type="search" placeholder="Enter keywords" />
+    </div>
+</div>
+<x-panel class="grid grid-cols-4 gap-4 mt-12 relative">
     @foreach($this->products as $product)
        <div class="bg-white rounded-lg shadow p-4 relative">
             <a href="{{ route('product', $product) }}" class="absolute inset-0 w-full h-full"></a>
@@ -8,8 +14,8 @@
             <span class="text-gray-700 text-sm">{{ $product->price }}</span>
        </div>
     @endforeach
-</div>
-<div>
+</x-panel>
+<div class="mt-6">
     {{ $this->products->links()}}
 </div>
 </div>
